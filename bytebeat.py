2c4ddeed410e15a5e3ba42aec37614105c13cbf4
kragen <kragen@VOSTRO9.(none)>
2012-05-05 13:41:15 -0300

    added a really lame pygame waveform display to python Tk bytebeat thingy

--- bytebeat.py
+++ bytebeat.py
@@ -1,6 +1,7 @@
 #!/usr/local/bin/python2.7
+# make font bigger
 
-import sys, wave, Tkinter, os, time, subprocess
+import sys, wave, Tkinter, os, time, subprocess, pygame
 
 # play_command = None
 # for candidate in ['/usr/bin/aplay', '/usr/bin/afplay']:
@@ -35,15 +36,17 @@ rate = 8000
 
 current_formula = lambda t: t
 t = 0
-interval = 100
-start = time.time()
-def send_frames(error, formula, out, outfd):
-    global current_formula, t
+interval = 33
+last_time = start = time.time() + 1
+def send_frames(error, formula, out, outfd, screen):
+    global current_formula, t, last_time
+    print time.time() - last_time,
+    sys.stdout.flush()
     try:
         formulas = formula.get().split(',')
         formulas[-1] = 'return ' + formulas[-1]
         ns = {}
-        exec "def new_formula(t):\n" + ''.join(" %s\n" % f for f in formulas) in ns
+        exec "def new_formula(t):\n" + ''.join(" %s\n" % f.strip() for f in formulas) in ns
         ns['new_formula'](0) & 255
         current_formula = ns['new_formula']
     except:
@@ -55,27 +58,47 @@ def send_frames(error, formula, out, outfd):
     needed = int(rate * (time.time() - start + 1.5*interval/1000.0)) - t
     while needed % 8 != 0:
         needed += 1
+    print needed,
+    # (t & 15) * (-t & 15) ^ (not (t & 16))-1 + 128
 
     output = []
     for i in range(needed):
         output.append(chr(current_formula(t) & 255))
         t += 1
+    
+    #canvas.delete('all')
+    screen.fill(0)
+    pygame.event.poll()
+    if len(output) > 1:
+        pygame.draw.lines(screen, (255, 255, 255), False,
+                          list(enumerate(map(ord, output))))
+    pygame.display.flip()
+
+        # canvas.create_rectangle(xx, yy, xx+4, yy+4, fill='grey50')
+        # if ii > 50:
+        #     break
+
     #out.writeframesraw(''.join(output))
     outfd.write(''.join(output))
     outfd.flush()
-    error.after(interval, lambda: send_frames(error, formula, out, outfd))
+    last_time = time.time()
+    error.after(interval, lambda: send_frames(error, formula, out, outfd, screen))
 
 def make_window():
     window = Tkinter.Tk()
+    bg = 'black'
+    print window.configure(background=bg)
     formula = Tkinter.StringVar()
-    formula.set('a = t * (t>>8 & 42), t >> 4')
-    entry = Tkinter.Entry(window, textvariable=formula)
+    formula.set('a = t * (t>>10 & 42), t >> 4')
+    entry = Tkinter.Entry(window, textvariable=formula, font='Monospace 32', background=bg, foreground='blue', insertbackground='blue')
     entry.pack(fill='x', side='top')
-    error = Tkinter.Label(window)
-    error.pack(fill='x')
+    entry.focus()
+    error = Tkinter.Label(window, font='VeraSans 32', background=bg, foreground='red')
+    error.pack(fill='x', side='top')
     #filename, out, outfd = new_wavfile()
     out = outfd = open('/dev/dsp', 'w')
-    send_frames(error, formula, out, outfd)
+    screen = pygame.display.set_mode((0, 0))
+    send_frames(error, formula, out, outfd, screen)
     #window.after(2*interval, lambda: subprocess.Popen([play_command, filename]))
     window.mainloop()
 
