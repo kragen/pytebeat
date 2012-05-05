37e2a3de0fbfdb9f9aa65308ada7a135d01605f4
kragen <kragen@VOSTRO9.(none)>
2012-05-05 15:28:46 -0300

    moved text field into an object

--- sdltextfield.py
+++ sdltextfield.py
@@ -2,10 +2,17 @@
 
 import pygame
 
-pygame.init()
-screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-font = pygame.font.Font(None, 96)
-hello = font.render('hello, world', 1, (255, 255, 255))
-screen.blit(hello, (100, 200))
-pygame.display.flip()
-pygame.time.delay(2000)
+class TextField(object):
+    def __init__(self, pos):
+        self.text = 'hello, world'
+        self.font = pygame.font.Font(None, 96)
+        self.pos = pos
+    def draw(self, surface):
+        surface.blit(self.font.render(self.text, 1, (255, 255, 255)), self.pos)
+
+if __name__ == '__main__': 
+    pygame.init()
+    screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+    TextField((100, 200)).draw(screen)
+    pygame.display.flip()
+    pygame.time.delay(2000)
