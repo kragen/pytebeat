a1cf5470f8325804c3310dcb690b7a5399cffd9e
user <user@localhost.localdomain>
2014-11-13 12:29:50 -0300

    added ALSA support

--- pytebeat.py
+++ pytebeat.py
@@ -5,7 +5,7 @@
 # - integer object has no attribute astype
 # - improve parse errors
 
-import sys, wave, os, time, subprocess, pygame, shuntparse, sdltextfield
+import sys, wave, os, time, subprocess, pygame, shuntparse, sdltextfield, errno
 
 try:
     from Numeric import array, arange, UInt8
@@ -103,7 +103,16 @@ def open_new_outfile():
         i += 1
 
 def make_window():
-    outfd = open('/dev/dsp', 'w')
+    try:
+        outfd = open('/dev/dsp', 'w')
+    except IOError, e:
+        if e.errno != errno.EACCES:
+            raise
+        # Probably we are on a system without Open Sound System, like
+        # most Linuxes of recent vintage.  Try invoking ALSA's aplay
+        # command instead.
+        outfd = os.popen('aplay -f U8', 'w')
+        
     outfile2 = open_new_outfile()
     outfd = Tee(outfd, outfile2)
     pygame.init()
