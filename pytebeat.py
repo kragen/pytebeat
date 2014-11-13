3f9a2f14992c8103e78838669ea1379a5ece13fb
user <user@localhost.localdomain>
2014-11-13 12:18:56 -0300

    added partial multiline to sdltextfield
    Doesn't display the selection yet if it's not on the first line, but
    at least it allows you to edit on subsequent lines!

--- pytebeat.py
+++ pytebeat.py
@@ -116,7 +116,8 @@ def make_window():
     formula = sdltextfield.TextField((10, 266), 
                                      foreground=(0,0,255),
                                      font=font,
-                                     text = 't >> 5 | t >> 4')
+                                     text = 't >> 5 | t >> 4',
+                                     width = screen.get_width() - 10)
     error = sdltextfield.TextField((10, 400), foreground=(255,0,0), focused=False, font=font)
     while True:
         run_mainloop(error, formula, outfd, screen)
